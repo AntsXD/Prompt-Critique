dcd39f0b9bf91dbc42744c02481df81cc33fe3e7
Anthony Charbel <anthonycharbel28@gmail.com>
2025-06-25 18:04:26 +0300

    For RailWay

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def health_check():
 
 if __name__ == '__main__':
     
-    port=int(os.environ.get("PORT",5000))
+    port=int(os.environ.get("PORT",8000))
     app.run(host='0.0.0.0',port=port,debug=False)
 
 
