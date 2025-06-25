6002cd9a585cb84361d6d917f691dd3238fc4498
Anthony Charbel <anthonycharbel28@gmail.com>
2025-06-25 14:49:58 +0300

    fixed port for render

--- main.py
+++ main.py
@@ -155,6 +155,7 @@ def health_check():
 
 if __name__ == '__main__':
     
-    app.run(host='0.0.0.0', port=10000, debug=False)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port,debug=False)
 
 
